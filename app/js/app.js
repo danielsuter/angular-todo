c3e864ee97ecba84cadd0bcad2201970e5124d32
Daniel Suter <suter_daniel@hotmail.com>
2014-02-04 18:29:12 +0100

    streamlined myApp to angular-todo

--- app/js/app.js
+++ app/js/app.js
@@ -2,12 +2,12 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
+angular.module('angular-todo', [
   'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
+  'angular-todo.filters',
+  'angular-todo.services',
+  'angular-todo.directives',
+  'angular-todo.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
--- app/js/filters.js
+++ app/js/filters.js
@@ -2,7 +2,7 @@
 
 /* Filters */
 
-angular.module('myApp.filters', []).
+angular.module('angular-todo.filters', []).
   filter('interpolate', ['version', function(version) {
     return function(text) {
       return String(text).replace(/\%VERSION\%/mg, version);
