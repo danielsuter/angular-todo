1c75594487bffd52387de9ed116542c9a65361b6
Daniel Suter <suter_daniel@hotmail.com>
2014-02-07 16:43:14 +0100

    added validation for create

--- frontend/app/js/filters.js
+++ frontend/app/js/filters.js
@@ -3,8 +3,16 @@
 /* Filters */
 
 angular.module('angular-todo.filters', []).
-  filter('interpolate', ['version', function(version) {
-    return function(text) {
-      return String(text).replace(/\%VERSION\%/mg, version);
-    }
-  }]);
+    filter('interpolate', ['version', function (version) {
+        return function (text) {
+            return String(text).replace(/\%VERSION\%/mg, version);
+        }
+    }]).filter('ja_nein', function () {
+        return function(flag) {
+            if (flag) {
+                return "Ja";
+            } else {
+                return "Nein";
+            }
+        }
+    });
