828233df825ade8427ced59f2ffd6fbb0c408425
Daniel Suter <suter_daniel@hotmail.com>
2014-02-13 16:07:23 +0100

    added test resource for resetting data

--- todo-backend/src/main/java/ch/daniel/todobackend/rest/TestResource.java
+++ todo-backend/src/main/java/ch/daniel/todobackend/rest/TestResource.java
@@ -8,6 +8,7 @@ import javax.ws.rs.core.Response.Status;
 
 import ch.daniel.todobackend.service.MailService;
 import ch.daniel.todobackend.service.NotifyService;
+import ch.daniel.todobackend.toremove.StartupEJB;
 
 @Path("test")
 public class TestResource {
@@ -18,9 +19,20 @@ public class TestResource {
 	@EJB
 	private NotifyService notifyService;
 	
+	@EJB
+	private StartupEJB startupEJB; 
+	
+	@Path("/sendmails")
 	@GET
 	public Response get() {
 		notifyService.sendMails();
+		return Response.status(Status.OK).build();
+	}
+	
+	@Path("/reset-test-data")
+	@GET
+	public Response resetTestData() {
+		startupEJB.contextInitialized();
 		return Response.status(Status.OK).build();
 	}
 }
